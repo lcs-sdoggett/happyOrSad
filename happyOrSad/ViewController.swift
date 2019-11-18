c7dd8c3c5173b5eee9aab530fed871ff4a31e5ee
Scott Doggett <sdoggett@lcs.on.ca>
2019-11-18 11:16:07 -0500

    Initial Commit

---
+++ happyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  happyOrSad
+//
+//  Created by Doggett, Scott on 2019-11-18.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
